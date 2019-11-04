7130b802afd044f62e52126bf99757eb19c52981
dizdaroglu <furkndizdaroglu@gmail.com>
2019-11-04 12:39:47 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
-import HomePage from "./homepage.component"
+import HomePage from "./pages/homepage/homepage.component"
+import { Route } from 'react-router-dom'
+
+const HatsPage = (props) => {
+
+  return (
+    < div >
+      <h1>hats PAGE</h1>
+    </div >
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/hats" component={HatsPage} />
+
+
     </div>
   );
 }
