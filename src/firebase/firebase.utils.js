58a99a52b7586c863e5363db3c2e6e9c1ce45a00
dizdaroglu <furkndizdaroglu@gmail.com>
2019-12-02 11:59:56 +0300

    updated

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-import { keys } from '../../config';
+import { keys } from '../config';
 
 const config = {
     apiKey: keys.apiKey,
@@ -57,6 +57,22 @@ export const addCollectionAndDocuments = async (
     return await batch.commit()
 }
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    })
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {})
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
